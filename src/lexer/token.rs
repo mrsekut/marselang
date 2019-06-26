add73188f16b3f668aadba3f7c42adbc3f4c2048
mrsekut <k.cloudspider@gmail.com>
2019-06-27 02:08:54 +0900

    [Add]eval

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Loc(pub usize, pub usize);
 
 impl Loc {
--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ impl UniOp {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum BinOpKind {
     Add,
     Sub,
@@ -176,9 +176,86 @@ fn parse_factor<Tokens: Iterator<Item = lexer::Token>>(
         })
 }
 
+pub struct Interpreter;
+
+impl Interpreter {
+    pub fn new() -> Self {
+        Interpreter
+    }
+
+    pub fn eval(&mut self, expr: &Ast) -> Result<u64, InterpreterError> {
+        use crate::AstKind::*;
+        match expr.value {
+            Num(n) => Ok(n),
+            BinOp {
+                ref op,
+                ref lhs,
+                ref rhs,
+            } => {
+                let l = self.eval(lhs)?;
+                let r = self.eval(rhs)?;
+                self.eval_binop(op, l, r)
+                    .map_err(|e| InterpreterError::new(e, expr.loc.clone()))
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn eval_binop(&mut self, op: &BinOp, lhs: u64, rhs: u64) -> Result<u64, InterpreterErrorKind> {
+        match op.value {
+            Add => Ok(lhs + rhs),
+            Sub => Ok(lhs - rhs),
+            Mul => Ok(lhs * rhs),
+            Div => {
+                if rhs == 0 {
+                    Err(InterpreterErrorKind::DivisionByZero)
+                } else {
+                    Ok(lhs / rhs)
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum InterpreterErrorKind {
+    DivisionByZero,
+}
+
+pub type InterpreterError = lexer::Annot<InterpreterErrorKind>;
+
+use std::error::Error as StdError;
+use std::fmt;
+
+impl fmt::Display for InterpreterError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::InterpreterErrorKind::*;
+        match self.value {
+            DivisionByZero => write!(f, "division by zero"),
+        }
+    }
+}
+
+impl StdError for InterpreterError {
+    fn description(&self) -> &str {
+        use self::InterpreterErrorKind::*;
+        match self.value {
+            DivisionByZero => "the right hand expression of the division evaluates to zero",
+        }
+    }
+}
+
+impl InterpreterError {
+    pub fn show_diagnostic(&self, input: &str) {
+        eprintln!("{}", self);
+        // print_annot(input, self.loc.clone());
+    }
+}
+
 fn main() {
-    let l = lexer("12+2").unwrap();
-    // let l = lexer("12 + (3 - 123) * 3 / 4").unwrap();
-    let p = parser(l);
-    println!("{:?}", p);
+    let mut interp = Interpreter::new();
+    let l = lexer::lexer("10+3*9").unwrap();
+    let p = parser(l).unwrap();
+    let i = interp.eval(&p);
+    println!("{:?}", i);
 }
